f93d5a77f386547e93eddb21b3894f63eea15e77
stankovictab <stankovictab@gmail.com>
2022-03-06 21:39:15 +0100

    new: Archimedes inital commit

---
+++ archimedes/eureka-client-one/src/main/java/archimedes/eurekaclientone/EurekaClientOneApplication.java
@@ -0,0 +1,22 @@
+package archimedes.eurekaclientone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientOneApplication {
+
+	@GetMapping(path = "/")
+	public String getHomepage() {
+		return "Client One Homepage";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientOneApplication.class, args);
+	}
+}
---
+++ archimedes/eureka-client-one/src/test/java/archimedes/eurekaclientone/EurekaClientOneApplicationTests.java
@@ -0,0 +1,13 @@
+package archimedes.eurekaclientone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
