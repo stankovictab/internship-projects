3633cd70fce28361f366c5dd2d484bed3f97a416
stankovictab <stankovictab@gmail.com>
2022-03-07 16:40:08 +0100

    bug: Importing FeignClient

--- archimedes/survey-service/src/main/java/archimedes/surveyservice/SurveyServiceController.java
+++ archimedes/survey-service/src/main/java/archimedes/surveyservice/SurveyServiceController.java
@@ -1,10 +1,16 @@
 package archimedes.surveyservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+// import archimedes.campaignservice.;
+import archimedes.campaignservice.CampaignServiceFeignClient;
 
 @RestController
 public class SurveyServiceController implements SurveyServiceFeignClient {
+
+	// @Autowired
+	// private CampaignServiceFeignClient csfc;
 
 	@GetMapping(value = "/")
 	@Override
