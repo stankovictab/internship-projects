2a53073d4b913bcf76749fa5e46766bb53b2a640
stankovictab <stankovictab@gmail.com>
2022-03-08 16:56:24 +0100

    fix: Fixed commit

--- archimedes/campaign-service/src/main/java/archimedes/campaignservice/Campaign.java
+++ archimedes/campaign-service/src/main/java/archimedes/campaignservice/Campaign.java
@@ -1,2 +1,14 @@
-package archimedes.campaignservice;public class Campaign {
+package archimedes.campaignservice;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Campaign {
+    @Id
+    private Long id;
+
+    private String campaignName;
 }
--- archimedes/campaign-service/src/main/java/archimedes/campaignservice/CampaignRepository.java
+++ archimedes/campaign-service/src/main/java/archimedes/campaignservice/CampaignRepository.java
@@ -1,2 +1,9 @@
-package archimedes.campaignservice;public interface CampaignRepository {
+package archimedes.campaignservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+    // TODO: ?
 }
